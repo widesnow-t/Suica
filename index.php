6af9c5d68f18cbbea74416b1736b3b9fec3117c7
widesnow-t <89765385+widesnow-t@users.noreply.github.com>
2021-12-14 04:31:39 +0900

    3commit

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
-$sic = 0;
-while($sic <= $money){
+do {
     echo "Suicaにチャージします。金額に該当する番号を入力してください" . "\n" . "1: 1,000円" . "\n" . "2: 3,000円" ."\n" . "3: 5,000円" . "\n" . "番号 :";
         $age = trim(fgets(STDIN));
         if ($age == 1 ){
@@ -17,16 +16,17 @@ while($sic <= $money){
             } else { echo "無効な番号です。" . "\n" . "処理を終了します。" . "\n";
                 break;
             }
-    
+    while($sic>0){
         echo "商品の価格を入力して下さい:";
         $money = trim(fgets(STDIN));
-        if ($sic >= $money){
-            $sic = $sic - $money;
+        if ($sic = $sic - $money > 0){
             echo "残高は" . $sic . "円です。" . "\n";
-        } else {
+        } 
+        else {
             echo "チャージ金額を上回るため購入できません。" . "\n";
             break;
         }
     }
+    } while ($sic>0);
     echo "買い物を終了します" . "\n";
 ?>
