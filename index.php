968a55124b62adf5fb67c4a6db85f9f0402d76c8
widesnow-t <89765385+widesnow-t@users.noreply.github.com>
2021-12-15 07:08:07 +0900

    5

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <?php
 echo "Suicaにチャージします。金額に該当する番号を入力してください" . "\n" . "1: 1,000円" . "\n" . "2: 3,000円" . "\n" . "3: 5,000円" . "\n" . "番号 :";
 $age = trim(fgets(STDIN));
-if ($age == 1) {
-    $sic = 1000;
-    echo "1,000円チャージされました。" . "\n";
-} elseif ($age == 2) {
-    $sic = 3000;
-    echo "3,000円チャージされました。" . "\n";
-} elseif ($age == 3) {
-    $sic = 5000;
-    echo "5,000円チャージされました。" . "\n";
-} else {
-    echo "無効な番号です。" . "\n" . "処理を終了します。" . "\n";
-    exit;
+switch ($age) {
+    case '1':
+        $sic = 1000;
+        echo "1,000円チャージされました。" . "\n";
+        break;
+    case '2':
+        $sic = 3000;
+        echo "3,000円チャージされました。" . "\n";
+        break;
+    case '3':
+        $sic = 5000;
+        echo "5,000円チャージされました。" . "\n";
+    default:
+        echo "無効な番号です。" . "\n" . "処理を終了します。" . "\n";
+        break;
 }
+
+$sic;
+
 while ($sic > 0) {
     echo "商品の価格を入力して下さい:";
     $money = trim(fgets(STDIN));
