d2e70772721a136c0c1f8333bda5487857eebc5f
widesnow-t <89765385+widesnow-t@users.noreply.github.com>
2021-12-14 21:12:54 +0900

    3

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php 
-do {
     echo "Suicaにチャージします。金額に該当する番号を入力してください" . "\n" . "1: 1,000円" . "\n" . "2: 3,000円" ."\n" . "3: 5,000円" . "\n" . "番号 :";
         $age = trim(fgets(STDIN));
         if ($age == 1 ){
-                $sic = $sic + 1000;
+                $sic = 1000;
                 echo "1,000円チャージされました。" . "\n";
         }
             elseif ($age == 2) {
-                $sic = $sic + 3000;
+                $sic = 3000;
                 echo "3,000円チャージされました。" . "\n";
             }
             elseif ($age == 3) {
-                $sic = $sic + 5000;
+                $sic = 5000;
                 echo "5,000円チャージされました。" . "\n";
             } else { echo "無効な番号です。" . "\n" . "処理を終了します。" . "\n";
-                break;
+                exit;
             }
-    while($sic>0){
+    while($sic > 0) {
         echo "商品の価格を入力して下さい:";
         $money = trim(fgets(STDIN));
-        if ($sic = $sic - $money > 0){
-            echo "残高は" . $sic . "円です。" . "\n";
+            if ($sic >= $money){
+                $sic = $sic - $money;
+            echo "残高は" . number_format($sic) . "円です。" . "\n";
         } 
         else {
             echo "チャージ金額を上回るため購入できません。" . "\n";
             break;
         }
-    }
-    } while ($sic>0);
+    };
+    
     echo "買い物を終了します" . "\n";
 ?>
